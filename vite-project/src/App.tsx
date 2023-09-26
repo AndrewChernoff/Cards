0dd16b27b7c6d25a90147ad62f5c515bd99768fd
Andrew Chernoff <whitedrew538@gmail.com>
2023-09-26 19:26:13 +0500

    feat: add styles

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div>Hello</div>
+  return (
+    <div>
+      <div>
+        <a href={'http://localhost:5173/hello'}>Link</a>
+      </div>
+      <Button as={'a'} href={'http://localhost:5173/hello'}>
+        Link
+      </Button>
+    </div>
+  )
 }
 
 export default App
