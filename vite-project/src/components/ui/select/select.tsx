ec622d1eeb1add10c638427241c8842a274100ce
Andrew Chernoff <whitedrew538@gmail.com>
2023-10-01 23:30:25 +0500

    rewrote select using other library need to change few styles

--- vite-project/src/components/ui/select/select.tsx
+++ vite-project/src/components/ui/select/select.tsx
@@ -1,143 +1,66 @@
-import { forwardRef } from 'react'
+import { useState } from 'react'
 
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
-import * as Select from '@radix-ui/react-select'
-import classnames from 'classnames'
-
-import './select.scss'
-
-const SelectDemo = () => (
-  <Select.Root>
-    <Select.Trigger className="SelectTrigger" aria-label="Food">
-      <Select.Value placeholder="Select a fruit…" />
-      <Select.Icon className="SelectIcon">
-        <ChevronDownIcon />
-      </Select.Icon>
-    </Select.Trigger>
-    <Select.Portal>
-      <Select.Content className="SelectContent">
-        <Select.ScrollUpButton className="SelectScrollButton">
-          <ChevronUpIcon />
-        </Select.ScrollUpButton>
-
-        {/* <Select.Group>
-            <Select.Label className="SelectLabel">Fruits</Select.Label>
-            <SelectItem value="apple">Apple</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-            <SelectItem value="blueberry">Blueberry</SelectItem>
-            <SelectItem value="grapes">Grapes</SelectItem>
-            <SelectItem value="pineapple">Pineapple</SelectItem>
-          </Select.Group>
-
-          <Select.Separator className="SelectSeparator" />
-
-          <Select.Group>
-            <Select.Label className="SelectLabel">Vegetables</Select.Label>
-            <SelectItem value="aubergine">Aubergine</SelectItem>
-            <SelectItem value="broccoli">Broccoli</SelectItem>
-            <SelectItem value="carrot" disabled>
-              Carrot
-            </SelectItem>
-            <SelectItem value="courgette">Courgette</SelectItem>
-            <SelectItem value="leek">Leek</SelectItem>
-          </Select.Group> */}
-
-        {/* <Select.Separator className="SelectSeparator" /> */}
-
-        <Select.Group className="group">
-          <Select.Label className="SelectLabel">Meat</Select.Label>
-          <SelectItem className={'sublabel'} value="beef">
-            Beef
-          </SelectItem>
-          <SelectItem className={'sublabel'} value="chicken">
-            Chicken
-          </SelectItem>
-          <SelectItem className={'sublabel'} value="lamb">
-            Lamb
-          </SelectItem>
-          <SelectItem className={'sublabel'} value="pork">
-            Pork
-          </SelectItem>
-        </Select.Group>
-        <Select.ScrollDownButton className="SelectScrollButton">
-          <ChevronDownIcon />
-        </Select.ScrollDownButton>
-      </Select.Content>
-    </Select.Portal>
-  </Select.Root>
-)
-
-const SelectItem = forwardRef(({ children, className, ...props }: any, forwardedRef) => {
-  return (
-    <Select.Item className={classnames('SelectItem', className)} {...props} ref={forwardedRef}>
-      <Select.ItemText>{children}</Select.ItemText>
-      <Select.ItemIndicator className="SelectItemIndicator">
-        <CheckIcon />
-      </Select.ItemIndicator>
-    </Select.Item>
-  )
-})
-
-export default SelectDemo
-
-/* import { forwardRef } from 'react'
-
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
-import * as Select from '@radix-ui/react-select'
-import classnames from 'classnames'
+import * as Radix from '@radix-ui/react-select'
 
 import s from './select.module.scss'
+import { SelTrigger, Dropdown, Wrapper, Viewport, Item } from './select_addition/select-components'
+import Chevron from './select_addition/shevron'
 
-const SelectDemo = () => (
-  <Select.Root>
-    <Select.Trigger className={s.SelectTrigger} aria-label="Food">
-      <Select.Value className={s.SelectValue} placeholder="Select a fruit…" />
-      <Select.Icon className={s.SelectIcon}>
-        <ChevronDownIcon />
-      </Select.Icon>
-    </Select.Trigger>
-    <Select.Portal>
-      <Select.Content className={s.SelectContent}>
-        <Select.ScrollUpButton className={s.SelectScrollButton}>
-          <ChevronUpIcon />
-        </Select.ScrollUpButton>
-        <Select.Viewport className={s.SelectViewport}>
-          <Select.Group>
-            <SelectItem className={s.sublabel} value="apple">
-              Apple
-            </SelectItem>
-            <SelectItem className={s.sublabel} value="banana">
-              Banana
-            </SelectItem>
-            <SelectItem className={s.sublabel} value="blueberry">
-              Blueberry
-            </SelectItem>
-            <SelectItem className={s.sublabel} value="grapes">
-              Grapes
-            </SelectItem>
-            <SelectItem className={s.sublabel} value="pineapple">
-              Pineapple
-            </SelectItem>
-          </Select.Group>
-        </Select.Viewport>
-        <Select.ScrollDownButton className={s.SelectScrollButton}>
-          <ChevronDownIcon />
-        </Select.ScrollDownButton>
-      </Select.Content>
-    </Select.Portal>
-  </Select.Root>
-)
+const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
 
-const SelectItem = forwardRef(({ children, className, ...props }: any, forwardedRef) => {
+const error = null
+
+const App = () => {
+  const [toggled, setToggled] = useState('closed')
+
   return (
-    <Select.Item className={classnames(s.SelectItem, className)} {...props} ref={forwardedRef}>
-      <Select.ItemText>{children}</Select.ItemText>
-      <Select.ItemIndicator className={s.SelectItemIndicator}>
-        <CheckIcon />
-      </Select.ItemIndicator>
-    </Select.Item>
-  )
-})
+    <div
+      style={{
+        /* height: '100vh',
+        background: 'linear-gradient(to right,#a6cad6,#d39c93)', */
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+      }}
+    >
+      <form style={{ width: '100%', maxWidth: 420 }}>
+        <Wrapper>
+          <Radix.Root
+            dir="ltr"
+            //open={true}
 
-export default SelectDemo
- */
+            defaultValue={items?.[0]}
+            onOpenChange={e => setToggled(e === true ? 'open' : 'closed')}
+          >
+            <Radix.Trigger asChild data-state={toggled}>
+              <SelTrigger error={!!error}>
+                <span>
+                  <Radix.Value />
+                </span>
+                <Radix.Icon asChild>
+                  <Chevron direction="down" />
+                </Radix.Icon>
+              </SelTrigger>
+            </Radix.Trigger>
+            <Radix.Content asChild>
+              <Dropdown style={{ marginTop: '50px' }}>
+                <Viewport>
+                  {items.map((item, i) => {
+                    return (
+                      <Item key={i} value={item}>
+                        <Radix.ItemText> {item} </Radix.ItemText>
+                      </Item>
+                    )
+                  })}
+                </Viewport>
+              </Dropdown>
+            </Radix.Content>
+          </Radix.Root>
+        </Wrapper>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/ui/select/select_addition/select-components.tsx
@@ -0,0 +1,71 @@
+import * as Radix from '@radix-ui/react-select'
+import { styled } from '@stitches/react'
+
+const Wrapper = styled('div', {
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+})
+
+const SelTrigger = styled('button', {
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  /* width: '100%', */
+  boxSizing: 'border-box',
+  background: '#2a2a2a',
+  padding: 16,
+  fontSize: 16,
+  fontFamily: 'sans-serif',
+  border: '1px solid #1b1b1b',
+  borderRadius: 4,
+  outline: 'none',
+  color: '#fff',
+  width: '210px',
+  height: '36px',
+  variants: {
+    error: {
+      true: {
+        borderColor: '#df6c75',
+      },
+    },
+  },
+})
+
+const Dropdown = styled('div', {
+  position: 'relative',
+  boxSizing: 'border-box',
+  color: '#fff',
+  padding: '16px 8px',
+  fontFamily: 'sans-serif',
+  fontSize: 16,
+  background: '#2a2a2a',
+  border: '1px solid #1b1b1b',
+  borderRadius: 4,
+  boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+  pointerEvents: 'all',
+  /* background: 'red', */
+})
+
+const Viewport = styled(Radix.Viewport, {
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: 8,
+  /* background: 'blue', */
+})
+
+const Item = styled(Radix.Item, {
+  padding: '8px',
+  outline: 'none',
+  transition: 'background ease 300ms',
+  borderRadius: 4,
+  /*  background: 'gold', */
+  width: '100%', ////
+  '&:focus': {
+    background: '#35363a',
+  },
+})
+
+export { Dropdown, SelTrigger, Wrapper, Viewport, Item }
